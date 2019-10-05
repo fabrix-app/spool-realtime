c72860915554e3e69af7452bdd5b89b1a0a4d3e0
Scott B. Wyatt <scott-wyatt@users.noreply.github.com>
2019-10-05 07:49:16 -0500

    chore(): fixes jsdoc

--- lib/api/controllers/PrimisController.ts
+++ lib/api/controllers/PrimisController.ts
@@ -3,6 +3,7 @@ import { FabrixController as Controller } from '@fabrix/fabrix/dist/common'
 /**
  * @module PrimusController
  * @description Primus Controller
+ */
 export class PrimusController extends Controller {
   spec(req, res) {
     return res.send(this.app.sockets.spec)
